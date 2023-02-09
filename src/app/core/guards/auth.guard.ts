81f66732b12a87485c5c30cc0c16f01428cd1928
MukundDholariya2706 <mukunddtridhyatech@gmail.com>
2023-02-09 12:19:15 +0530

    remove tslint

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -6,21 +6,16 @@ import { AuthenticationService } from 'src/app/auth/service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  /**
-   *
-   * @param {Router} _router
-   * @param {AuthenticationService} _authenticationService
-   */
-  constructor(private _router: Router, private _authenticationService: AuthenticationService) {}
+  constructor(private router: Router, private authenticationService: AuthenticationService) {}
 
   // canActivate
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const currentUser: User = this._authenticationService.getActiveUser();
+    const currentUser: User = this.authenticationService.getActiveUser();
     if (currentUser) {
       // check if route is restricted by role
       if (route.data.roles && route.data.roles.indexOf(currentUser.role) === -1) {
         // role not authorised so redirect to not-authorized page
-        this._router.navigate(['dashboard']);
+        this.router.navigate(['dashboard']);
         return false;
       }
 
@@ -29,7 +24,7 @@ export class AuthGuard implements CanActivate {
     }
 
     // not logged in so redirect to login page with the return url
-    this._router.navigate(['/auth/login']);
+    this.router.navigate(['/auth/login']);
     return false;
   }
 }
